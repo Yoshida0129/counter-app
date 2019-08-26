b88670d9bbe9ae3f903e57744d9bad2ca8c04383
Tatsuya Yoshida <alpacagame0129@gmail.com>
2019-08-26 13:11:39 +0900

    Fix: comment 修正
    ここもスプレッドシートからで良かった気が

--- src/components/molecules/event.js
+++ src/components/molecules/event.js
@@ -5,8 +5,9 @@ export const eventContainer = () => {
   return(
     <section>
       <h2>開始時間</h2>
-      {appText('2019/08/25 22:19', 18)}
-      {appText('~現在プレイ中~', 18)}
+      {appText('2019/08/25 22:19 ~ ', 18)}
+      {appText('~ 2019/08/26 8:30', 18)}
+      {appText('playerが寝落ち、PS4をdが持って帰った為、終了', 18)}
     </section>
   )
 }
