0c5b1736e68ef562d1f6a1e3cbc38fde25b24ac5
Yoshida0129 <alpacagame0129@gmail.com>
2021-04-16 17:56:54 +0900

    delete tournament

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import BloodBorne from './components/pages/BloodBorne'
 import DarkSoul from './components/pages/Darksoul3'
-import Tournament from './components/pages/Tournament'
 
 export default class App extends Component {
   render() {
@@ -11,7 +10,6 @@ export default class App extends Component {
       <BrowserRouter>
         <Route exact path="/" component={BloodBorne}/>
         <Route exact path="/darksoul_3" component={DarkSoul}/>
-        <Route path="/tournament" component={Tournament}/>
       </BrowserRouter>
     )
   }
